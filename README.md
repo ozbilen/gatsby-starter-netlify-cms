7ed33fd88c631fa132b70d154bb51dd33e30df0e
ozbilen <ozbilenweb@gmail.com>
2020-04-19 23:22:27 +0300

    ok

--- README.md
+++ README.md
@@ -83,7 +83,7 @@ CMS.registerPreviewTemplate('blog', BlogPostPreview)
 Note: Don't forget to also remove them from `package.json` and `yarn.lock` / `package-lock.json` using `yarn` or `npm`. During the build netlify-cms-app will bundle the media libraries as well, having them removed will save you build time.
 Example:
 ```
-yarn remove netlify-cms-media-library-uploadcare
+yarn remove netlify-cms-media-library-uploadcareno
 ```
 OR
 ```
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Özbilen Kız Kur'an Kursu" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -61,32 +61,22 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                Hakkımızda
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Faaliyetlerimiz
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                Kursumuz
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                Bize Ulaşın
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
+          
+             
             </div>
             <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+             
             </div>
           </div>
         </div>
